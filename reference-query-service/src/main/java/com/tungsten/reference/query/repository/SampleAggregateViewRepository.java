697b2ba326150e412451c7784b783bec7cafc44d
Premanand C <premanandc@gmail.com>
2017-06-10 23:20:55 -0400

    Enabled exposure of query views as a RESTful resource

--- reference-query-service/src/main/java/com/tungsten/reference/query/repository/SampleAggregateViewRepository.java
+++ reference-query-service/src/main/java/com/tungsten/reference/query/repository/SampleAggregateViewRepository.java
@@ -2,6 +2,17 @@ package com.tungsten.reference.query.repository;
 
 import com.tungsten.reference.query.view.SampleAggregateView;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+@RepositoryRestResource
 public interface SampleAggregateViewRepository extends PagingAndSortingRepository<SampleAggregateView, String> {
+
+  @RestResource(exported = false)
+  @Override
+  SampleAggregateView save(SampleAggregateView entity);
+
+  @RestResource(exported = false)
+  @Override
+  void delete(String id);
 }
